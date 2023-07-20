f8752b899e0d4edd7880a1f80fc67cd46781b7b4
Xolile <109028631+RemarkableAlly@users.noreply.github.com>
2023-07-20 14:21:58 +0200

    Update script.js
    I have changed the script to get the user currently logged on the pc and the domain. This will limit the admin of adding users on the domain list and having to select which domain they are on, commented on the script for better understanding

--- script.js
+++ script.js
@@ -48,41 +48,41 @@ addBtn.addEventListener('click', addInput);
 
 // Create a batch file that completes the task
 
-const runas = "runas /profile /env /user:"
-
+//const runas = "runas /profile /env /user:" //run as a domain users for access to the shared folder
 const jobRun = "xcopy /y ";
-const createDirectory = " /i";
+const createDirectory = " /i"; //creates a folder if it does not exist in on the default dir
 const deleteFile = "\ndel "
-const Drive = '"C:\\Users\\'
-const deletePath = '\\Downloads\\'
+const userLocation = '%USERPROFILE%' //Get the user's default path
+const deletePath = '\\Downloads\\' //the fodler with the bat file, used to delete when the file is done running
 const deleteFileName = '\*Run To Copy*'
 const backSlash = "\\"
-const defaultPath = "C:\\Users\\"
 const dedaultPathFinal = "\\Documents\\"
+const getDomain = '%USERDOMAIN%\\' //domain of the network is stored by this const, this is part of the .bat script
+const getUser = '%USERNAME%' //username of current logged in user is stored by this line, this is part of the .bat script
 
 function createBatFile(){
-    const user = document.getElementById("userAD").value;
-    const source = document.getElementById("server").value;
-    const domain = document.getElementById("domain").value;
+    const source = document.getElementById("server").value; //user assisted share folder path, this is filled in as the path of the shared folder
 
     let data = [];
     for (let i = 0; i < counter; i++) {
-        const road = document.getElementById("path" + i).value;
-        const target = document.getElementById("recording" + i).value;
-        const dest = document.getElementById("destination" + i).value;
+        const road = document.getElementById("path" + i).value; //source of the file is stored by this line, user input filled
+        const target = document.getElementById("recording" + i).value; //filename is stored by this line, user input filled
+        const dest = document.getElementById("destination" + i).value; //destination of the copied file is store on this line, user input filled
 
         data.push(
-            jobRun + '\"' + source + road + "*" + target + '*"' + " " + '\"'  +defaultPath + user + dedaultPathFinal + dest + backSlash + '\"' + createDirectory
+            jobRun + '\"' + source + road + "*" + target + '*"' + " " + '\"'  + userLocation + dedaultPathFinal + dest + backSlash + '\"' + createDirectory //the file gets copied from the source then pasted to the targeted folder, the default folder is Documents
         );
     };
     data.push(
-        deleteFile + Drive + user + deletePath + deleteFileName + '\"'
+        deleteFile + userLocation + deletePath + deleteFileName + '\"' //delete the file downloaded, this is to get the download folder clean
     );
 
+    //create a file to be downloaded
     const data_string = data.join('\n');
-    const file = new Blob([runas + domain + user + "\n" + data_string], {type: "text/plain"});
+    //const file = new Blob([runas + getDomain + getUser + "\n" + data_string], {type: "text/plain"}); //only use if you want to run as a different user or domain user
+    const file = new Blob([data_string], {type: "text/plain"});
     const anchor = document.createElement("a");
     anchor.href = URL.createObjectURL(file);
-    anchor.download = "Run To Copy.txt";
+    anchor.download = "Automate Job Copy.bat";
     anchor.click();
 };
